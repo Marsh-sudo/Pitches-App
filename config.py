0bf05f9b9282885c3d4dd97300a5433c5ac9fe72
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-05 21:50:11 +0300

    add mail configurations

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://munyao:kevo12@localhost/pitches'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #EMAIL configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
