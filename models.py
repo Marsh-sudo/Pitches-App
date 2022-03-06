51692da08359299ecee2b15b655b2b2f0905bce8
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-06 21:48:57 +0300

    defined relationship of user

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from operator import index
 from .app import db
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
 from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
@@ -14,10 +14,12 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+    upvotes = db.relationship('upvote',backref = 'user',lazy = "dynamic")
+    downvotes = db.relationship('downvote' ,backref = 'user',lazy="dynamic")
+    pitch = db.relationship('Pitch',backref = 'user', lazy = "dynamic")
 
     @property
     def password(self):
@@ -42,7 +44,6 @@ class Pitch(db.Model):
     description = db.Column(db.String(255))
     category = db.Column(db.String(255))
     comments = db.relationship('Comment',backref = 'pitch',lazy = "dynamic")
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
     downvotes = db.relationship('downvotes',backref = 'pitch',lazy = "dynamic")
     upvotes = db.relationship('upvotes',backref = 'pitch',lazy = "dynamic")
 
@@ -64,14 +65,16 @@ class Upvote(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     upvote = db.Column(db.Integer,default = 0)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
 
     def save_upvotes(self):
         db.session.add(self)
         db.session.commit()
 
     def add_upvotes(cls,id):
-        upvote = Upvote(pitch_id=id)
+        upvote = Upvote(pitch_id=id ,user= current_user)
         return upvote
 
     @classmethod
@@ -81,4 +84,30 @@ class Upvote(db.Model):
 
 
     def __repr__(self):
-        return f'{self.pitch_id}'
+        return f'{self.pitch_id} : {self.user_id}'
+
+
+class Downvotes(db.Model):
+
+    __tablename__= 'downvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    downvote = db.Column(db.Integer,default = 0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+    def save_downvotes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_downvotes(cls,id):
+        downvote = Downvotes(pitch_id=id,user = current_user)
+        return downvote
+
+    @classmethod
+    def get_downvotes(cls,id):
+        downvotes = Downvotes.query.filter_by(pitch_id=id).all()
+        return downvotes
+
+    def __repr__(self):
+        return f'{self.pitch_id} : {self.user_id}'
