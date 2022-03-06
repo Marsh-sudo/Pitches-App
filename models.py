033c7845e72e42ff54f65bb5d72ec0f59d1c285c
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-06 19:01:16 +0300

    Add a upvote class

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from operator import index
-from . import db
+from .app import db
 from flask_login import UserMixin
 from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -33,12 +34,51 @@ class User(UserMixin,db.Model):
         return  f'User {self.username}'
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
 
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy = "dynamic")
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    comments = db.relationship('Comment',backref = 'pitch',lazy = "dynamic")
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    downvotes = db.relationship('downvotes',backref = 'pitch',lazy = "dynamic")
+    upvotes = db.relationship('upvotes',backref = 'pitch',lazy = "dynamic")
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitch = Pitch.query.filter_by(pitch_id=id).all
+        return pitch
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f'User {self.description}'
+
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    upvote = db.Column(db.Integer,default = 0)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+    def save_upvotes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_upvotes(cls,id):
+        upvote = Upvote(pitch_id=id)
+        return upvote
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvotes = Upvote.query.filter_by(pitch_id=id).all()
+        return upvotes
+
+
+    def __repr__(self):
+        return f'{self.pitch_id}'
