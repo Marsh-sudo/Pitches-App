1f9217917ba7ddfb5c390b5a7410057373a54ccc
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-07 15:20:54 +0300

    import email module

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,12 @@
 from crypt import methods
 from turtle import title
 from flask import render_template,redirect,url_for ,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
 from . import auth
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -29,6 +30,16 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Pitches","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
