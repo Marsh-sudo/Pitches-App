700f621d844b39df4da0c5877c5238cbca6ab545
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-07 15:18:48 +0300

    import app

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 import app
-from models import User,Role
+from models import Pitch, User
 
 manager = Manager(app)
 migrate = Migrate(app,db)
@@ -11,6 +11,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db, User = User,Role = Role)
+    return dict(app = app,db = db, User = User,Pitch = Pitch)
 if __name__ == '__main__':
     manager.run()
